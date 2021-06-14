0158656b76b0166dcb715cf725ee15f8db1f8fd2
cub525 <49500036+cub525@users.noreply.github.com>
2021-06-14 12:06:23 -0600

    Feat offline mode

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,35 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.1.5/workbox-sw.js');
+// Choose a cache name
+const cacheName = 'cache-v1';
+// List the files to precache
+const precacheResources = [
+    '/rosharan-music/',
+    '/rosharan-music/index.html',
+    '/rosharan-music/styles/index.css',
+    '/rosharan-music/manifest.json',
+    '/rosharan-music/images/icons/icon-512x512.png',
+    '/rosharan-music/scripts/audiosynth.js',
+    "/rosharan-music/scripts/playKeyboard.js"
+    ];
 
-workbox.routing.registerRoute(
-    ({ request }) => request.destination === "image",
-    new workbox.strategies.CacheFirst()
-);
+// When the service worker is installing, open the cache and add the precache resources to it
+self.addEventListener('install', (event) => {
+  console.log('Service worker install event!');
+  event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(precacheResources)));
+});
 
-workbox.routing.registerRoute(
-    ({ url }) => url.pathname === '/index.html',
-    new workbox.strategies.NetworkFirst()
-);
+self.addEventListener('activate', (event) => {
+  console.log('Service worker activate event!');
+});
+
+// When there's an incoming fetch request, try and respond with a precached resource, otherwise fall back to the network
+self.addEventListener('fetch', (event) => {
+  console.log('Fetch intercepted for:', event.request.url);
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return fetch(event.request);
+    }),
+  );
+});
